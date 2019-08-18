5261970da235c5821b2d562d4116a2e8c3b77e08
Derk Norton <derk.norton@gmail.com>
2019-08-18 13:52:00 -0600

    Added parse() and format() functions to the compiler interface.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@
 
 const Compiler = require('./src/Compiler').Compiler;
 const Assembler = require('./src/Assembler').Assembler;
-const bytecode = require('./src/utilities').bytecode;
+const utilities = require('./src/utilities');
 
 
 /**
@@ -28,12 +28,13 @@ exports.api = function(debug) {
     return {
 
         /**
-         * This function compiles the source code for a procedure into a compilation context
-         * containing the corresponding assembly instructions for the Nebula Virtual Processor.
+         * This function compiles the Bali Nebula™ source code for a procedure into a compilation
+         * context containing the corresponding Bali Nebula™ machines instructions.
          * 
-         * @param {Catalog} type The type context for the document being compiled.
-         * @param {Source} procedure The source code for the procedure.
-         * @returns {Catalog} The compiled context for the procedure.
+         * @param {Catalog} type A catalog containing the type context for the procedure being
+         * compiled.
+         * @param {Source} procedure The source code for the procedure being compiled.
+         * @returns {Catalog} A catalog containing the compiled procedure context.
          */
         compile: function(type, procedure) {
             const compiler = new Compiler(debug);
@@ -42,53 +43,94 @@ exports.api = function(debug) {
         },
 
         /**
-         * This function assembles the instructions in a compiled procedure into the corresponding
-         * bytecode to be run on the Nebula Virual Processor.
+         * This function assembles the Bali Nebula™ machine instructions contained in a compiled
+         * procedure context into the corresponding bytecode which is added to the compiled
+         * procedure context.
          * 
-         * @param {Catalog} type The type context for the document being compiled and assembled.
-         * @param {Catalog} context The compilation context for the procedure being assembled.
+         * @param {Catalog} type A catalog containing the type context for the procedure being
+         * assembled.
+         * @param {Catalog} procedure A catalog containing the compiled procedure context.
          */
-        assemble: function(type, context) {
+        assemble: function(type, procedure) {
             const assembler = new Assembler(debug);
-            assembler.assembleProcedure(type, context);
-        },
-
-        bytecode: function(bytes) {
-            return bytecode.bytesToBytecode(bytes);
-        },
-
-        bytes: function(bytecode) {
-            return bytecode.bytecodeToBytes(bytecode);
+            assembler.assembleProcedure(type, procedure);
         },
 
         /**
-         * This function decodes the operation for an instruction.
+         * This function formats a list of Bali Nebula™ machine instructions into a JavaScript
+         * string containing the corresponding Bali Nebula™ assembly code. An optional
+         * indentation level may be specified that causes the formatter to indent each line by
+         * that many additional levels.  Each level is four spaces and the default is zero levels.
+         * 
+         * @param {List} instructions The list of machine instructions to be formatted.
+         * @param {Number} indentation An optional number of levels to indent the output.
+         * @returns {String} A string containing the corresponding assembly code.
+         */
+        format: function(instructions, indentation) {
+            const formatter = new utilities.Formatter(indentation);
+            return formatter.formatInstructions(instructions);
+        },
+
+        /**
+         * This function parses a string containing Bali Nebula™ assembly code. It generates
+         * the corresponding list of Bali Nebula™ machine instructions.
+         * 
+         * @param {String} assembly A string containing Bali Nebula™ assembly code.
+         * @returns {List} A list containing the corresponding Bali Nebula™ machine instructions.
+         */
+        parse: function(assembly) {
+            const parser = new utilities.Parser(debug);
+            return parser.parseAssembly(assembly);
+        },
+
+        /**
+         * This function converts a buffer of bytes into a bytecode array.
+         * 
+         * @param {Buffer} bytes A buffer of bytes.
+         * @returns {Array} An array containing the corresponding bytecode.
+         */
+        bytecode: function(bytes) {
+            return utilities.bytecode.bytesToBytecode(bytes);
+        },
+
+        /**
+         * This function converts a bytecode array into a buffer of bytes.
+         * 
+         * @param {Array} bytecode An array containing the bytecode.
+         * @returns {Buffer} A buffer containing the corresponding bytes.
+         */
+        bytes: function(bytecode) {
+            return utilities.bytecode.bytecodeToBytes(bytecode);
+        },
+
+        /**
+         * This function decodes the operation for a Bali Nebula™ machine instruction.
          *
          * @param {Number} instruction The instruction to be decoded.
          * @return {Number} The decoded operation.
          */
         operation: function(instruction) {
-            return bytecode.decodeOperation(instruction);
+            return utilities.bytecode.decodeOperation(instruction);
         },
 
         /**
-         * This function decodes the modifier for an instruction.
+         * This function decodes the modifier for a Bali Nebula™ machine instruction.
          *
          * @param {Number} instruction The instruction to be decoded.
          * @return {Number} The decoded modifier.
          */
         modifier: function(instruction) {
-            return bytecode.decodeModifier(instruction);
+            return utilities.bytecode.decodeModifier(instruction);
         },
 
         /**
-         * This function decodes the operand for an instruction.
+         * This function decodes the operand for a Bali Nebula™ machine instruction.
          *
          * @param {Number} instruction The instruction to be decoded.
          * @return {Number} The decoded operand.
          */
         operand: function(instruction) {
-            return bytecode.decodeOperand(instruction);
+            return utilities.bytecode.decodeOperand(instruction);
         }
 
     };
